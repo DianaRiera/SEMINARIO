ccb1341f56aa385c476a6f7668bd27d097958184
Diana Riera <rieradiana22@gmail.com>
2019-07-10 01:00:09 -0600

    creando proyecto

---
+++ swtipoexamenplanta-master/routes/api/plantas.js
@@ -0,0 +1,83 @@
+var express = require('express');
+var router = express.Router();
+var ObjectID = require('mongodb').ObjectID;
+
+function initPlanta(db) {
+  var plantasColl = db.collection('plantas');
+  router.get('/', (req, res, next)=>{
+    plantasColl.find().toArray((err, plantas)=>{
+      if(err){
+        console.log(err);
+        return res.status(404).json({"error":"Error al extraer plantas de la base de datos"});
+      }
+      return res.status(200).json(plantas);
+    });
+  }); // get all
+  router.get('/:id', (req, res, next)=>{
+    var id = new ObjectID(req.params.id);
+    plantasColl.findOne({"_id": id} , (err, doc)=>{
+      if(err){
+        console.log(err);
+        return res.status(404).json({"error":"No se Puede Obtener Planta Intente de Nuevo"});
+      }
+      return res.status(200).json(doc);
+    });//findOne
+  }); // /:id
+
+  router.post('/', (req, res, next)=>{
+    var newPlanta = Object.assign(
+      {},
+      {
+        "nombre":"",
+        "tipo":"",
+        "taxonomia":"",
+        "uso":"",
+        "fechaCreated": new Date().getTime(),
+        "views":0,
+        "likes":0
+      },
+      req.body
+    );
+    plantasColl.insertOne(newPlanta, (err, rslt)=>{
+      if(err){
+        console.log(err);
+        return res.status(404).json({"error":"No se pudo agregar nueva planta"});
+      }
+      if(rslt.ops.length===0){
+        console.log(rslt);
+        return res.status(404).json({ "error": "No se pudo agregar nueva planta" });
+      }
+      return res.status(200).json(rslt.ops[0]);
+    });
+  });//post
+
+  router.put('/:id', (req, res, next)=>{
+    var query = {"_id":new ObjectID(req.params.id)};
+    var update = {"$inc":{"views":1, "likes":1}};
+
+    plantasColl.updateOne(query, update, (err, rslt)=>{
+      if (err) {
+        console.log(err);
+        return res.status(404).json({ "error": "No se pudo modificar planta" });
+      }
+      
+      return res.status(200).json(rslt);
+    })
+  }); // put
+
+  router.delete('/:id', (req, res, next) => {
+    var query = { "_id": new ObjectID(req.params.id) };
+    plantasColl.removeOne(query, (err, rslt) => {
+      if (err) {
+        console.log(err);
+        return res.status(404).json({ "error": "No se pudo eliminar planta" });
+      }
+
+      return res.status(200).json(rslt);
+    })
+  }); // delete
+
+  return router;
+}
+
+module.exports = initPlanta;
